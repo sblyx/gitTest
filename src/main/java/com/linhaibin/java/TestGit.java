2503e47c2be70e9235b220284b2c295f23dc6c40
lin <lin@qq.com>
2022-07-25 21:57:10 +0800

    first commit

---
+++ src/main/java/com/linhaibin/java/TestGit.java
@@ -0,0 +1,11 @@
+package com.linhaibin.java;
+
+/**
+ * @author lin
+ * @create 2022-07-25-21:55
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("Hello,Git");
+    }
+}
