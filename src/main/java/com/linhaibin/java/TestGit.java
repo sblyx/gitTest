a46f74174908bd9479851185d4e5ddf90704ac4d
sblyx <66558236+sblyx@users.noreply.github.com>
2022-07-26 13:55:25 +0800

    Update TestGit.java

--- src/main/java/com/linhaibin/java/TestGit.java
+++ src/main/java/com/linhaibin/java/TestGit.java
@@ -8,5 +8,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello,Git");
         System.out.println("test push");
+        System.out.println("test pull");
     }
 }
