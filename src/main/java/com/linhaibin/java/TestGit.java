697b5a3b67c571d463c5d006a9aed9601a437e2d
“lin” <lin@qq.com>
2022-07-26 14:35:54 +0800

    test4 commit

--- src/main/java/com/linhaibin/java/TestGit.java
+++ src/main/java/com/linhaibin/java/TestGit.java
@@ -14,6 +14,7 @@ public class TestGit {
         System.out.println("test pull");
         System.out.println("test pull");
         System.out.println("test li ");
+        System.out.println("test li ");
 
 
 
