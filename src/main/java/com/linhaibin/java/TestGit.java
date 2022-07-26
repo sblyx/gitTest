e8a7fba86d505a9c279cab1e24c1bf6c76d1fc0d
lin <lin@qq.com>
2022-07-26 13:21:12 +0800

    second commit

--- src/main/java/com/linhaibin/java/TestGit.java
+++ src/main/java/com/linhaibin/java/TestGit.java
@@ -7,6 +7,8 @@ package com.linhaibin.java;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello,Git");
-        System.out.println("Hello,Git2");
+        System.out.println("Hello,Git3");
+
+
     }
 }
