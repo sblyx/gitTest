090bd44c4c4ad490b4b3e7e309ba3754b9b1e1c7
lin <lin@qq.com>
2022-07-26 14:17:13 +0800

    test commit

--- src/main/java/com/linhaibin/java/TestGit.java
+++ src/main/java/com/linhaibin/java/TestGit.java
@@ -8,5 +8,7 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello,Git");
         System.out.println("test push");
+
+        System.out.println("shishi");
     }
 }
